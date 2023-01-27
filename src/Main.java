f37c41a869c7994c4879ecad52ba4a2d26efeb5f
milenaalmazyan <ams007@mail.ru>
2023-01-28 01:05:49 +0300

    HW_10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
     }
+    public static void task1() {
+        System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String lastName = "Ivanov";
+        String middleName = "Ivanovich";
+        String fullName = firstName + " " + lastName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+    }
+    public static void task2() {
+        System.out.println("Задача 2");
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        String fullName = "Иванов Семён Семёнович";
+        String fullName1 = fullName.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника — " + fullName1);
+    }
+
 }
